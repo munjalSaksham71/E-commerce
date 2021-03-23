e18a4da5c7702a7b6a440900ffd567a4c12b5f12
munjalSaksham71 <sakshammunjal4@gmail.com>
2021-03-23 14:50:40 +0530

    prepare for deploying

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from "./config/db.js";
@@ -8,7 +9,7 @@ import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 
-
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json())
 
@@ -17,15 +18,22 @@ dotenv.config();
 connectDB()
 
 const PORT = process.env.PORT||5000;
-app.get('/',(req,res) => {
-    res.send("API is running...")
-});
-
 app.use('/api/products', productRoutes);    
 app.use('/api/users', userRoutes);    
 app.use('/api/orders', orderRoutes);
 app.use('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 app.use(notFound)
 app.use(errorHandler)
